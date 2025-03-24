878dcecaf43a754ce58d02e50b44b6392042e78d
Alysson M. Bruno <alysson.mb@unitins.br>
2025-03-23 23:16:31 -0300

    Implementado o CRUD

---
+++ LivroRepository.java
@@ -0,0 +1,9 @@
+package br.untins.biblioteca.livro;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends CrudRepository<Livro, Long> {
+
+}
