6e8da5d1d105dd9774be4f51f552dbe34b34bf8f
Alysson M. Bruno <alysson.mb@unitins.br>
2025-03-24 02:19:57 +0000

    Removendo arquivos e add conf. do codespace

--- LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.untins.biblioteca.livro;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends CrudRepository<Livro, Long> {
-
-}
